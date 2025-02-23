789fc79d0b22001b8f66f2c5ffa5afc2bcc6bda7
BradLeneus <150571637+BradLeneus@users.noreply.github.com>
2025-02-23 11:57:52 -0500

    Finalisation du style Css de la Navbar selon la maquette

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-
-function Navbar(props) {
+import '../Css/Navbar.css'
+function Navbar() {
     return (
         <div>
-            <nav className="navbar fixed-top navbar-light bg-light justify-content-between">
-                <text className="m-2">CryptoVault</text>
+            <nav className="navbar fixed-top navbar justify-content-between" id={"navBar"}>
+                <svg width="180" height="56" viewBox="0 0 180 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path id="Ellipse 1" d="M0.679412 25.7887C207.616 30.9031 41.4171 -0.712913 90.8315 0.508356C140.246 1.72963 180 15.0274 179.625 30.2113C179.249 45.3952 138.887 56.7129 89.4726 55.4916C103.648 28.3323 0.304144 40.9726 0.679412 25.7887Z" fill="#395CCF"/>
+                </svg>
+
+                <text className="m-2" id="nomApp">CryptoVault</text>
                 <form className="form-inline">
 
 
                     <Link to="/">
-                        <button className="btn m-2 btn-outline-success my-2 my-sm-0" type="submit">Home</button>
+                        <text className={"pagesName"}>Home</text>
                     </Link>
+                    &nbsp;
                     <Link to="/Trading">
-                        <button className="btn m-2 btn-outline-success my-2 my-sm-0" type="submit">Trading Price
-                        </button>
+                        <text className={"pagesName"}>Trading Prices</text>
                     </Link>
+                    &nbsp;
                     <Link to="/Wallet">
-                        <button className="btn m-2 btn-outline-success my-2 my-sm-0" type="submit">My Wallet</button>
+                      <text className={"pagesName"}>My Wallet</text>
                     </Link>
+                    &nbsp;
                     <Link to="/About">
-                        <button className="btn m-2 btn-outline-success my-2 my-sm-0" type="submit">About</button>
+                        <text className={"pagesName"}>About</text>
+                    </Link>
+                    &nbsp;
+
+
+                    <Link to="/Login">
+                        <button className="m-2 my-2 " type="button">Login</button>
                     </Link>
                     <Link to="/SignIn">
-                        <button className="btn m-2 btn-outline-success my-2 my-sm-0" type="submit">Sign in</button>
-                    </Link>
-                    <Link to="/Login">
-                        <button className="btn m-2 btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+                        <button className="m-2 my-2 " type="button">Sign In</button>
                     </Link>
 
 
